50096346772677d43405f43457bfade9844f7db0
Aaditya <aadityanitin@gmail.com>
2022-04-02 20:49:28 +0530

    Finished the project

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser")
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname+ "/index.html")
+});
+
+app.post("/", function(req, res){
+    const query = req.body.cityName
+    const apiKey = "60c2590ea06f0ede7318e186b2441e9f"
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appid="+ apiKey +"&units=metric"
+    https.get(url, function(response){
+        console.log(response.statusCode);
+
+        response.on("data", function(data){
+            const weatherData = JSON.parse(data)
+            console.log(weatherData);
+            const temp = weatherData.main.temp
+            // console.log(temp);
+            const desc = weatherData.weather[0].description
+            // console.log(desc);
+            const icon = weatherData.weather[0].icon
+            const imgURL= "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+            res.write("<h1>Temprature in " + query +" is " + temp + " degree celcius<h1>");
+            res.write("<p>It feels "+ desc + " outside</p>");
+            res.write("<img src=" + imgURL +">");
+            res.send()
+        })
+    })
+})
+
+
+
+
+app.listen(3000, function(){
+    console.log("Server is running on Server 3000")
+})
